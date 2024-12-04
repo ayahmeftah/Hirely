62a2a09512cccc533273808248d848c0620aaf97
AreejYusuf23 <areeg33@hotmail.com>
2024-12-04 17:29:39 +0300

    CVBuilder

---
+++ Hirely/Hirely/Controllers/CVEducationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CVEducationViewController.swift
+//  Hirely
+//
+//  Created by BP-36-201-01 on 04/12/2024.
+//
+
+import UIKit
+
+class CVEducationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
