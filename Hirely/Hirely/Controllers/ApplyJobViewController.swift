e94a99d4f5a9761dd17e0dedf560efedb5805dd3
khabdulla198 <khadijahasan198@gmail.com>
2024-12-24 17:05:58 +0300

    outlets done

---
+++ Hirely/Hirely/Controllers/ApplyJobViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ApplyJobViewController.swift
+//  Hirely
+//
+//  Created by BP-36-201-01 on 24/12/2024.
+//
+
+import UIKit
+
+class ApplyJobViewController: UIViewController {
+
+    
+    @IBOutlet weak var fullNametxt: UITextField!
+    
+    @IBOutlet weak var agetxt: UITextField!
+    
+    @IBOutlet weak var phoneNumbertxt: UITextField!
+    
+    @IBOutlet weak var emailtxt: UITextField!
+    
+    @IBOutlet weak var uploadCVbtn: UIButton!
+    
+    @IBOutlet weak var uploadCoverLetterbtn: UIButton!
+    
+    
+    @IBOutlet weak var applybtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
