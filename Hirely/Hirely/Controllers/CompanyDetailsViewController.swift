59f89096a6949c77cf3d8f0605656576a76ae908
khabdulla198 <khadijahasan@gmail.com>
2024-12-25 16:17:08 +0300

    company details

---
+++ Hirely/Hirely/Controllers/CompanyDetailsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CompanyDetailsViewController.swift
+//  Hirely
+//
+//  Created by BP-needchange on 25/12/2024.
+//
+
+import UIKit
+
+class CompanyDetailsViewController: UIViewController {
+
+    
+    
+    
+    @IBOutlet weak var cNamelbl: UILabel!
+    
+    @IBOutlet weak var cEmaillbl: UILabel!
+    
+    
+    @IBOutlet weak var cOverviewtxt: UITextView!
+    
+    @IBOutlet weak var cSpecializationtxt: UITextView!
+    
+    @IBOutlet weak var cGrowthDevtxt: UITextView!
+    
+    @IBOutlet weak var cBenefitCultxt: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Hirely/Hirely/structs/CompanyDetails.swift
@@ -0,0 +1,9 @@
+//
+//  CompanyDetails.swift
+//  Hirely
+//
+//  Created by BP-needchange on 25/12/2024.
+//
+
+import Foundation
+import FirebaseFirestore
